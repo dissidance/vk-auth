797dac224e780fd3b39d7bba7c7d3ac830da0116
Max Zhivaev <dizas@me.com>
2020-07-24 22:52:57 +0300

    Updates

---
+++ precache-manifest.3479419a32bd49cfa980a37e6e42c24d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd648a6ba672b27c19c839eca0e3a1fb",
+    "url": "/vk-auth/index.html"
+  },
+  {
+    "revision": "b64249d963b9893856ba",
+    "url": "/vk-auth/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a105bef29ba8e486ffd0",
+    "url": "/vk-auth/static/js/2.d8b274bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vk-auth/static/js/2.d8b274bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b64249d963b9893856ba",
+    "url": "/vk-auth/static/js/main.f9da8c96.chunk.js"
+  },
+  {
+    "revision": "f1ddd5e1dbf8561742be",
+    "url": "/vk-auth/static/js/runtime-main.3dabbbef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vk-auth/static/media/logo.5d5d9eef.svg"
+  }
+]);
